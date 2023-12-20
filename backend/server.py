544d6a0391c824ca752561f3b25921169d8dc60c
Raphael-06 <rjtvsalinas2019@plm.edu.ph>
2023-12-20 13:38:28 +0800

    changed api call and added few data

--- backend/server.py
+++ backend/server.py
@@ -5,15 +5,15 @@ import requests
 
 app = Flask(__name__)
 
-api_endpoint = 'https://fakerapi.it/api/v1/persons?_quantity=1'
+api_endpoint = 'https://fakerapi.it/api/v1/custom?_quantity=1&firstName=firstName&lastName=lastName&street=streetAddress&city=city&country=country&phone=phone&company=company_name&dateEntered=dateTime&dateRetired=dateTime&school=state&dateEntered=dateTime&dateGraduated=dateTime&certification=website'
 
 def process_api_response(response):
     if response.status_code == 200:
         json_data = response.json()
         candidate_data = extract_data(json_data)
-        response = summarize_data(candidate_data)
+        # response = summarize_data(candidate_data)
 
-        return {"response":response}
+        return candidate_data
     else:
         return jsonify({'error': 'Failed to fetch data from API'}), response.status_code
 
@@ -22,9 +22,15 @@ def process_api_response(response):
 def handle_api_data():
     try:
         # Make a GET request to the API
-        response = requests.get(api_endpoint)
+        count = 0
+        list_of_response = []
+        while count < 4:
+            response = requests.get(api_endpoint)
+            processed_response = process_api_response(response)
+            list_of_response.append(processed_response)
+            count += 1
         # Process the API response
-        return process_api_response(response)
+        return list_of_response
     except requests.RequestException as e:
         return jsonify({'error': str(e)}), 500
 
