760a76ad77570c21eacbdd069bf270ad3dc2b780
Charles Noah <noahimperium@gmail.com>
2024-01-18 13:50:10 +0800

    update/drag-and-drop
    - Added drag and drop function

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, {useState, useRef} from 'react'
+import { useCandidate } from '../context/Context';
 import PDFInfo from './PDFInfo'
 import axios from 'axios';
 
@@ -8,6 +9,17 @@ const Sidebar = ({data}) => {
   const [inputValue, setInputValue] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [missingDataToSearch, setMissingDataToSearch] = useState(null)
+
+  const handleDrag = (event) => {
+    event.preventDefault()
+
+  }
+  const handleDrop = (event) => {
+    event.preventDefault()
+    setSelectedFile(event.dataTransfer.files[0])
+
+
+  }
 
   const handleMissingDataSearch = (event) => {
     setMissingDataToSearch(event.target.value)
@@ -26,6 +38,7 @@ const Sidebar = ({data}) => {
       .then(response => {
         // Handle the successful response
         setSearchResults(response.data);
+        console.log(response.data)
       })
       .catch(error => {
         // Handle errors here
@@ -69,26 +82,44 @@ const Sidebar = ({data}) => {
   return (
     <div className='flex justify-center items-center w-1/4 flex-col h-full'>
     {/* Specialized Nav */}
-    <div className='border-solid border-b-2 border-[#E7E7E7] w-full px-2 py-2'>
+        <div className='border-solid border-b-2 border-[#E7E7E7] w-full px-2 py-2'>
             <img src={require('../img/specialized_icon.png')} alt='specialized-icon' className='w-1/2'/>
         </div>
     {/* Pdf Info */}
         <div className='flex justify-center items-center flex-col p-3 w-[80%] px-4 border-solid border-b-2 border-[#E7E7E7] '>
-                <div className='w-[100%] flex flex-col gap-'>
+                <div className='w-[100%] flex flex-col'>
                     <div className='rounded-sm border-dotted border-2 border-[#E7E7E7] w-[100%] h-[20vh] flex flex-col justify-center items-center p-10 gap-2'>                    
                         <div className='flex flex-col justify-center items-center'
-                        onClick={handleDivClick}>
-                            <img src={require('../img/upload.jpg')} alt='upload'></img>
-                            <input
-                            type="file"
-                            accept="application/pdf"
-                            name="pdfFile"
-                            className='upload-button hidden w-full' 
-                            onChange={handleFileChange}
-                            ref={fileInputRef} />
-                            <label id="upload-text">
-                                <span class="text-[.75rem] text-[#919191]" id="upload-click">Upload a CV from your computer</span>
-                            </label>
+                        onClick={handleDivClick}
+                        onDragOver={handleDrag}
+                        onDrop={handleDrop}>
+                          {selectedFile ? 
+                          <>
+                          <div className='flex flex-row justify-center items-center gap-3 w-full '>
+                            <div className='rounded-full bg-[#D3D3D3] w-[15%] flex justify-center items-center p-2'>
+                              <img src={require('../img/pdf_icon.png')} alt='pdf-icon' className='w-[80%]'/>
+                            </div>
+                              <p className='text-[.75rem] min-w-[60%] max-w=[60%] break-words'>{selectedFile.name}</p> 
+                        
+
+                          </div>
+                            
+                          </>
+                          : <> 
+                              <img src={require('../img/upload.jpg')} alt='upload'></img>
+                              <input
+                              type="file"
+                              accept="application/pdf"
+                              name="pdfFile"
+                              className='upload-button hidden w-full' 
+                              onChange={handleFileChange}
+                              ref={fileInputRef} />
+                              <label id="upload-text">
+                                  <span class="text-[.75rem] text-[#919191]" id="upload-click">Upload a CV from your computer</span>
+                              </label>
+                            </>
+                          }
+                            
                             
                         </div>
                         
@@ -99,7 +130,7 @@ const Sidebar = ({data}) => {
             </div>
     {/* Search Bar  */}
         <div className='flex justify-center items-center flex-col gap-2 w-[80%] py-4 border-solid border-b-2 border-[#E7E7E7]'>
-        <label className='text-[.80rem] text-[#8F8F8F]'>Search by name</label>
+        <label className='text-[.80rem] text-[#8F8F8F] w-full text-left'>Search by name</label>
             <div className='border-solid border-2 border-[#E7E7E7] w-full flex flex-row justify-between items-center gap-4 p-2'>
                 <input 
                   value={inputValue}
@@ -112,7 +143,7 @@ const Sidebar = ({data}) => {
         </div>
 
         <div className='flex justify-center items-center flex-col gap-2 w-[80%] py-4 border-solid border-b-2 border-[#E7E7E7]'>
-            <label className='text-[.80rem] text-[#8F8F8F]'>Search by missing data</label>
+            <label className='text-[.80rem] text-[#8F8F8F] w-full text-left'>Search by missing data</label>
             <div className='border-solid border-2 border-[#E7E7E7] w-full flex flex-row justify-between items-center gap-4 p-2'>
               <select className='w-full' value={missingDataToSearch} onChange={handleMissingDataSearch}>
               <option value="age">Age</option>
