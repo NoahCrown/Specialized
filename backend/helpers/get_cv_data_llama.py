b2a6146333a44a1fdd286650628c7db162600686
Charles Noah <noahimperium@gmail.com>
2024-02-07 16:18:08 +0800

    tweak/update_prompts

--- backend/helpers/get_cv_data_llama.py
+++ backend/helpers/get_cv_data_llama.py
@@ -40,6 +40,10 @@ def extract_cv(pdf_file):
     init_query_translation = '' if lang == 'en' else f'Suppose you are a {lang} to English translator and the document is in {lang} texts, can you translate it so that you can extract the data and read it without giving me an output of translated texts and then'
 
     candidate_query = """
+        <<SYS>>
+        You are a bot who is professional at extracting candidate data from a candidate's resume.
+        <<SYS>>
+        [INST]
         This is a CV containing candidate's information: {text}
         Follow this format and insert the proper information as values. 
         Do not copy the example values given to you. If you cannot find the value, just put 'None' as the value and don't put the example value provided in the example json:(Only send me/ return the data list and nothing else).
@@ -63,6 +67,7 @@ def extract_cv(pdf_file):
                 'specialties': {{ 'data': [ '(Specialties of the candidate)' ], 'total': (Total specialties of the candidate) }}
             }}
         Again, do not copy and paste the values. If you cannot find or undentify the value or keys just put the value as None.
+        [/INST]
     """
 
     query = init_query_translation + candidate_query
@@ -73,43 +78,53 @@ def extract_cv(pdf_file):
     }
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     response_candidate = llm_chain.run(text)
-    # json_start = response.find('{')
-    # json_end = response.rfind('}') + 1
+    json_start = response_candidate.find('{')
+    json_end = response_candidate.rfind('}') + 1
 
-    # response = response[json_start:json_end]
+    response = response_candidate[json_start:json_end]
+    print(eval(response))
+
     # response = parse_json_with_autofix(response)
 
     workhistory_query = """
+        <<SYS>>
+        You are a bot who is professional at extracting candidate's work history data from a candidate's resume.
+        <<SYS>>
+        
+        [INST]
         This is a CV containing candidate's information: {text}
         Follow this format and insert the proper information as values. 
         Do not copy the example values given to you. If you cannot find the value, just put 'None' as the value and don't put the example value provided in the example json:(Only send me/ return the data list and nothing else).
-        Be accurate with the data. For example, in the address don't put in the address if it is not totally clear for you to identify. Just put "None" as the value, if so.
+        Be accurate with the data. For example, in the address don't put in the address if it is not totally clear for you to identify. Just put None as the value, if so.
         You should return a valid dictionary following the format below. Remember to plug in the datas as value. Remember to put all of the work history available.
             
-            {{
-                [{{
-                'comments': '(Work Description/summary)',
-                'companyName': '(Company name of the work in candidate's work history)',
-                'endDate': (end date of work experience in epoch timestamp),
-                'id': (Work experience ID),
-                'isLastJob': '(Is this candidate's last job if yes True, if not False)',
-                'jobOrder': None,
-                'startDate': '(Start date of work history the candidate work's history )',
-                'title': '(candidate's work title in his last job)'
-                }}]  
-            }}
+            {{"workHistory": [{{
+                                'comments': '(insert here candidate's work description)',
+                                'companyName': '(insert here candidate's company name of the work in candidate's work history)',
+                                'endDate': (insert here end date of work experience in epoch timestamp),
+                                'id': (insert here the candidate's work experience ID),
+                                'isLastJob': '(insert here Is this candidate's last job if yes True, if not False)',
+                                'jobOrder': None,
+                                'startDate': '(insert here Start date of work history the candidate work's history )',
+                                'title': '(insert here the candidate's work title in his last job)'}}]}}
         Again, do not copy and paste the values. If you cannot find or undentify the value or keys just put the value as None.
+        [/INST]
+
     """
 
     workhistory_query = init_query_translation + workhistory_query
     prompt = PromptTemplate(template=workhistory_query, input_variables=["text"])
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     response_workhistory = llm_chain.run(text)
-    # json_start = response.find('{')
-    # json_end = response.rfind('}') + 1
+    json_start = response_workhistory.find('{')
+    json_end = response_workhistory.rfind('}') + 1
 
-    # response = response[json_start:json_end]
+    response = response_workhistory[json_start:json_end]
+    print(response_workhistory)
+    print(eval(response))
+
     # response = parse_json_with_autofix(response)
 
     response = [response_candidate, response_workhistory]
+
     return response
