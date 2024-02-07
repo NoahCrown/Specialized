4a125c59d6ffac98f404fc53479d7cbf7a630ecd
Charles Noah <noahimperium@gmail.com>
2024-02-07 12:28:16 +0800

    tweak/adjusted_infer_prompts

--- backend/prompts/llama_prompts.py
+++ backend/prompts/llama_prompts.py
@@ -29,31 +29,14 @@ def language_skill(candidate_data, custom_prompt):
     Data: 
     {candidate_data}
 
-    Json Format:
-    [
-    {{
-        "Language":"English",
-        "enProficiency":( insert here how proficient the candidate is in english (en) ranging from None/Basic/Conversational/Business/Fluent/Native)),
-        "confidence: ( insert confidence level here from 1-5, 1 being the lowest),
-    }},
-    {{
-        "Language":"Japanese",
-        "jpProficiency":( insert here how proficient the candidate is in japanese (jp) ranging from None/Basic/Conversational/Business/Fluent/Native)),
-        "confidence:( insert confidence level here from 1-5, 1 being the lowest),
-    }}
-    ]
-
     Only return me a json object, no explanation, no sample data, no nothing, just the json data with values inserted in it.
+    Json Format:
 
-    [/INST]
+
     
 """
 
     json_format= """
-    Don't add an explanation only send back the json data
-    Please follow the json format provided below and only return the json data
-
-    JSON Format:
     [
     {{
         "Language":"English",
@@ -66,8 +49,11 @@ def language_skill(candidate_data, custom_prompt):
         "confidence:( insert confidence level here from 1-5, 1 being the lowest),
     }}
     ]
+
+    [/INST]
+
     """
-    query = custom_prompt + load_data 
+    query = custom_prompt + load_data + json_format
     prompt = PromptTemplate(template=query, input_variables=["candidate_data"])
     llm = DeepInfra(model_id = "meta-llama/Llama-2-70b-chat-hf", verbose=True)
     llm.model_kwargs = {
@@ -108,19 +94,18 @@ def infer_age(candidate_data, custom_prompt, current_date):
     Current Date: 
     {current_date}
 
-    Json Format:
-    {{"Age": (insert here the inferred age of the candidate base on the data given to you), "confidence": (AI's confidence in inferring the data 1-5, 5 being the highest),}}
-
     Only return me a json object, no explanation, no sample data, no nothing, just the json data with values inserted in it.
+    Json Format:
 
-    [/INST]
+    
 
     """
 
     json_format = """
     {{"Age": (insert here the inferred age of the candidate base on the data given to you), "confidence": (AI's confidence in inferring the data 1-5, 5 being the highest),}}
+    [/INST]
     """
-    query = load_data 
+    query = load_data +json_format
     prompt = PromptTemplate(template=query, input_variables=["candidate_data", "custom_prompt", "current_date"])
     params = {"candidate_data":candidate_data, "custom_prompt": custom_prompt, "current_date": current_date}
     llm = DeepInfra(model_id = "meta-llama/Llama-2-70b-chat-hf", verbose=False)
@@ -162,24 +147,21 @@ def infer_location(candidate_data, custom_prompt, current_date):
     Current Date: 
     {current_date}
 
-    Json Format:
-    {{"Location":( insert here the inferred location of the candidate base on the data given to you ),"confidence":( AI's confidence in inferring the data 1-5, 5 being the highest)}}
-    
     Only return me a json object, no explanation, no sample data, no nothing, just the json data with values inserted in it.
-    [/INST]
+    Json Format:
+    
 
     """
 
     json_format= """
-    Don't add an explanation only send back the json data
-    Please follow the json format provided below and only return the json data
-
     {
         "Location":  '( insert here the inferred location of the candidate base on the data given to you )',
         "confidence": ( AI's confidence in inferring the data 1-5, 5 being the highest)
     }
+    [/INST]
+
     """
-    query = custom_prompt + load_data
+    query = custom_prompt + load_data + json_format
     prompt = PromptTemplate(template=query, input_variables=["candidate_data", "current_date"])
     params = {"candidate_data":candidate_data, "current_date": current_date}
     llm = DeepInfra(model_id = "meta-llama/Llama-2-70b-chat-hf", verbose=False)
